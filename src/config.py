183b070c392eddf8fcc030478f8375cd76c5e965
tielmac <tiel0119@student.uu.se>
2026-03-16 18:36:26 +0100

    Update config.py
    Updated config.py file

--- src/config.py
+++ src/config.py
@@ -20,7 +20,7 @@ YEAR_COL = "year"
 #   WORDS TO ANALYZE
 # =========================
 
-WORDS = ["trump", "game", "movie", "love", "news"]
+WORDS = ["WORD1", "WORD2", "WORD3", "WORD4", "WORD5"]
 
 # =========================
 #   SPARK SESSION
